9481b7b46abcb4dda5aef82e5e5137b65ddeb501
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-03-03 00:22:57 +0000

    Update scala-library, scala-reflect to 2.13.13

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -18,7 +18,7 @@ object ProjectDependencies {
 
   object Core {
     val dedicated: Seq[ModuleID] = Seq(
-      "org.scala-lang" % "scala-reflect" % "2.13.12"
+      "org.scala-lang" % "scala-reflect" % "2.13.13"
     )
   }
 
