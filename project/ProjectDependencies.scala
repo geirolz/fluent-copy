8f337dd5cfee59bbd4acc81fbebb8cae7b072b72
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-12-08 00:11:26 +0000

    Update munit to 1.0.3

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -9,7 +9,7 @@ object ProjectDependencies {
 
   lazy val common: Seq[ModuleID] = Seq(
     // TEST
-    "org.scalameta"  %% "munit"            % "1.0.2"  % Test,
+    "org.scalameta"  %% "munit"            % "1.0.3"  % Test,
     "org.scalameta"  %% "munit-scalacheck" % "1.0.0"  % Test,
     "org.typelevel"  %% "cats-laws"        % "2.12.0" % Test,
     "org.typelevel"  %% "discipline-munit" % "2.0.0"  % Test,
